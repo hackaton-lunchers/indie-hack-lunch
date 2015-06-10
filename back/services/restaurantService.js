04410a942e235869fb5652fb73ee18c154bb1496
Mikuláš Balabán <balaban@cngroup.dk>
2015-06-10 13:56:40 +0200

    changed details in parsing specific for each restaurants; removed alergens

--- back/services/restaurantService.js
+++ back/services/restaurantService.js
@@ -57,7 +57,7 @@ class RestaurantService{
 
     }
 
-    parserMenickaCZ(resolve, reject, err, resp, body_1250){
+    parserMenickaCZ(resolve, reject, err, resp, body_1250, menuUrl){
         var menu = [];
         //[{"title":"bucek", "description":"nejlepsi chuti", "price":"120Kc", "type": "soup"},{}]
         var body_utf8 = iconv.decode(body_1250, 'win1250');
@@ -87,25 +87,57 @@ class RestaurantService{
                         if (page(this).next().hasClass('cena')) {
                             price = page(this).next().text()
                         }
-                        var title = String(page(this).text()).replace('\t','');
-                        var description = '';
-                        if(title.includes("-") || title.includes("–") || title.includes("/")){
-                            var parts = title.split(/[-–/]+/)
-                            //console.log('0:' + parts[0] + '  1:' + parts[1]);
-                            if(parts.length == 2){
-                                title = parts[0];
-                                description = parts[1];
-                            }
-                            if(parts.length == 3){
-                                title = parts[0]+'-'+parts[1]; //Kuře bang-bang
-                                description = parts[2];
-                            }
-                            //console.log('    i: ' + i + ' text:' + page(this).text() + ' class:' + page(this).attr('class'));
-                        }
+
                         var type = '';
                         if(page(this).attr('class').includes("capitalize")) {
                             type = 'soup';
                         }
+
+                        var title = String(page(this).text()).replace('\t','');
+
+                        if(menuUrl.includes("u-dzberu") || menuUrl.includes("the-pub") || menuUrl.includes("vaclavka")){
+                            //console.log('    ' + title);
+                            var pos = title.indexOf("\A-");
+                            if(pos == -1){
+                                pos = title.indexOf("(A");
+                            }
+                            if(menuUrl.includes("vaclavka")){
+                                pos = title.indexOf('(');
+                            }
+                            if(pos>-1){
+                                title = title.substring(0, pos-1); //remove alergens e.g. "/A-1,3,7"
+                                //console.log('    ' + title);
+                            }
+                        }
+
+                        var description = '';
+                        if(type != 'soup'){  //Uzená s kroupami / Čočková s klobásou
+                            if(title.includes("-") || title.includes("–") || title.includes("/")){
+                                var parts = title.split(/[-–/]+/)
+                                //console.log('0:' + parts[0] + '  1:' + parts[1]);
+                                if(parts.length == 2 && !menuUrl.includes("the-pub")){
+                                    title = parts[0];
+                                    description = parts[1];
+                                }
+                                if(parts.length == 3){
+                                    title = parts[0]+'-'+parts[1]; //Kuře bang-bang
+                                    description = parts[2];
+                                }
+                                //console.log('    i: ' + i + ' text:' + page(this).text() + ' class:' + page(this).attr('class'));
+                            }
+                        }
+
+                        if(menuUrl.includes("vaclavka")){
+                            if(title.includes('Polévka:') || title.includes('Hlavní jídla:') || title.includes('AKCE, RAUTY, VEČÍRKY') || title.includes('Uspořádáme večírek')){
+                                return true; //continue;
+                            }
+                        }
+                        if(menuUrl.includes("na-blbym-miste")){
+                            if(title.includes('Kompletní menu:') || title.includes('Vegetariánská nabídka:') || title.includes('Dezert:')){
+                                return true; //continue;
+                            }
+                        }
+
                         console.log('title: ' + title + '  description: ' + description +'  price: ' + price + '  type: ' + type);
 
                         menu.push({
@@ -135,7 +167,7 @@ class RestaurantService{
 				function (err, resp, body) {
                     console.log("menuUrl: " + menuUrl);
                     if(menuUrl.includes("menicka.cz")){
-                        self.parserMenickaCZ(resolve, reject, err, resp, body);
+                        self.parserMenickaCZ(resolve, reject, err, resp, body, menuUrl);
                     }
 
 				}
